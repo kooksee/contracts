80b625ed9265c4e65f7dca628dc5357caabae73d
白云辉 <baiyunhui@yuanben.org>
2018-07-25 20:16:57 +0800

    初始化基础的智能合约

---
+++ contracts/SafeMath.sol
@@ -0,0 +1,33 @@
+pragma solidity ^0.4.14;
+
+library SafeMath {
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        if (a == 0) {
+            return 0;
+        }
+        uint256 c = a * b;
+        assert(c / a == b);
+        return c;
+    }
+
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        assert(b > 0); // Solidity automatically throws when dividing by 0
+        uint256 c = a / b;
+        assert(a == b * c);
+        return c;
+    }
+
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a - b;
+        assert(b <= a);
+        assert(a == c + b);
+        return c;
+    }
+
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        assert(c >= a);
+        assert(a == c - b);
+        return c;
+    }
+}
